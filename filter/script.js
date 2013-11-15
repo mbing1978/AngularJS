f56f518612c82909551e6bf880f501af286ecfbe
mbing1978 <11070519@163.com>
2013-11-15 17:20:59 +0800

    angularjs.org

--- filter/script.js
+++ filter/script.js
@@ -1,12 +1,18 @@
-angular.module('FilterInControllerModule', []).
-  controller('FilterController', ['filterFilter', function(filterFilter) {
-    this.array = [
-      {name: 'Tobias'},
-      {name: 'Jeff'},
-      {name: 'Brian'},
-      {name: 'Igor'},
-      {name: 'James'},
-      {name: 'Brad'}
-    ];
-    this.filteredArray = filterFilter(this.array, 'a');
-  }]);
+angular.module('MyReverseModule', []).
+  filter('reverse', function() {
+    return function(input, uppercase) {
+      var out = "";
+      for (var i = 0; i < input.length; i++) {
+        out = input.charAt(i) + out;
+      }
+      // conditional based on optional argument
+      if (uppercase) {
+        out = out.toUpperCase();
+      }
+      return out;
+    }
+  });
+
+function Ctrl($scope) {
+  $scope.greeting = 'hello';
+}
