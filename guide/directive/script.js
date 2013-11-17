b6b1cf6ef8d6d455eef57f52f39997c271a39593
mbing1978 <11070519@163.com>
2013-11-17 15:46:08 +0800

    angularjs.org

--- guide/directive/script.js
+++ guide/directive/script.js
@@ -1,11 +1,36 @@
-angular.module('docsTransclusionDirective', [])
-  .controller('Ctrl', function($scope) {
-    $scope.name = 'Tobias';
-  })
-  .directive('myDialog', function() {
-    return {
-      restrict: 'E',
-      transclude: true,
-      templateUrl: 'my-dialog.html'
-    };
+angular.module('dragModule', []).
+  directive('myDraggable', function($document) {
+    return function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0;
+
+      element.css({
+        position: 'relative',
+        border: '1px solid red',
+        backgroundColor: 'lightgrey',
+        cursor: 'pointer'
+      });
+
+      element.on('mousedown', function(event) {
+        // Prevent default dragging of selected content
+        event.preventDefault();
+        startX = event.pageX - x;
+        startY = event.pageY - y;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(event) {
+        y = event.pageY - startY;
+        x = event.pageX - startX;
+        element.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
+      }
+    }
   });
